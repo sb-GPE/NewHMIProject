057916fee734ec5114368ab56038faf1924527b7
sb-GPE <107251320+sb-GPE@users.noreply.github.com>
2022-07-21 12:54:13 +0200

    Add files via upload

---
+++ ProjectFiles/NetSolution/Private/TypeConstants.cs
@@ -0,0 +1,20 @@
+using System;
+using UAManagedCore;
+
+//-------------------------------------------
+// WARNING: AUTO-GENERATED CODE, DO NOT EDIT!
+//-------------------------------------------
+
+namespace NewHMIProject
+{
+    public static class ObjectTypes
+    {
+        private static readonly int namespaceIndex = NamespaceMapProvider.GetNamespaceIndex("NewHMIProject");
+        public static readonly NodeId MainWindow = new NodeId(namespaceIndex, new Guid("695b3ca174e6b83e547f8203cf206be6"));
+    }
+
+    public static class VariableTypes
+    {
+        private static readonly int namespaceIndex = NamespaceMapProvider.GetNamespaceIndex("NewHMIProject");
+    }
+}
---
+++ ProjectFiles/NetSolution/Private/UITypeDefinitions.cs
@@ -0,0 +1,11 @@
+using System;
+using UAManagedCore;
+
+//-------------------------------------------
+// WARNING: AUTO-GENERATED CODE, DO NOT EDIT!
+//-------------------------------------------
+
+[MapType(NamespaceUri = "NewHMIProject", Guid = "695b3ca174e6b83e547f8203cf206be6")]
+public class MainWindow : FTOptix.UI.Window
+{
+}
